0d04477ea3be2cb697c3f2e91dea66e59a50a145
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-09-14 12:37:51 +0200

    init commit changes

--- src/Task1.java
+++ src/Task1.java
@@ -7,18 +7,23 @@
  */
 public class Task1 {
     public static void main(String[] args) {
-        tariff(100);
+        tariff(-100);
+        tariff(0);
+        tariff(3);
         tariff(1000);
         tariff(3000);
 
     }
+
     public static void tariff(int minutesPerMonth) {
-        if (minutesPerMonth < 500) {
+        if (minutesPerMonth>0 && minutesPerMonth < 500) {
             System.out.println("Эконом");
         } else if (minutesPerMonth >= 500 && minutesPerMonth <= 1000) {
             System.out.println("Стандарт");
-        } else {
+        } else if (minutesPerMonth > 1000) {
             System.out.println("Премиум");
+        }else {
+            System.out.println("Для вашего запроса нет тарифа");
         }
     }
 }
